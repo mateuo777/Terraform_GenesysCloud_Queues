cafc6cc73865b1a165b36bb431fe74003d9e1ece
Mateusz Janowicz <ext.mateusz.janowicz@infinit.cx>
2025-03-06 08:56:59 +0100

    Adding two initial files

---
+++ resources.tf
@@ -0,0 +1,7 @@
+resource "genesyscloud_tf_export" "include-filter" {
+  directory = "./gc_users"
+  export_as_hcl = false
+  log_permission_errors = true
+  include_state_file = true
+  include_filter_resources = ["genesyscloud_user"]
+}
