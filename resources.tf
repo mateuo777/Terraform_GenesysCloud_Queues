0d56dca64767fd8628bc0cfa9b15d043f7dae1d4
mateuo777 <37333049+mateuo777@users.noreply.github.com>
2025-03-06 15:30:49 +0100

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "genesyscloud_tf_export" "include-filter" {
   directory = "./genesyscloud"
-  export_as_hcl = true
+  export_as_hcl = false
   log_permission_errors = true
   include_state_file = true
   include_filter_resources = ["genesyscloud_user"]
