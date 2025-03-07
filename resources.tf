f0422f0c8c46277f9c7bcad89317198c96b33805
mateuo777 <37333049+mateuo777@users.noreply.github.com>
2025-03-07 14:07:00 +0100

    Delete resources.tf

--- resources.tf
+++
@@ -1,6 +0,0 @@
-resource "genesyscloud_tf_export" "include-filter" {
-  export_as_hcl = true
-  log_permission_errors = true
-  include_state_file = true
-  include_filter_resources = ["genesyscloud_user"]
-}
